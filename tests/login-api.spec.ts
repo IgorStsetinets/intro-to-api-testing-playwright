4618f9f5ecd4bada3b671d441564f81ee079d17d
igors <igorstsetinets@gmail.com>
2024-11-09 09:48:59 +0200

    Added JWT checks and negative test for invalid HTTP method

--- tests/login-api.spec.ts
+++ tests/login-api.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from '@playwright/test'
 import { LoginDto } from './dto/login-dto'
+import { StatusCodes } from 'http-status-codes'
 
 const serviceURL = 'https://backend.tallinn-learning.ee/'
 const loginPath = 'login/student'
@@ -7,20 +8,28 @@ const loginPath = 'login/student'
 test.describe('Tallinn delivery API tests', () => {
   test('login with correct data', async ({ request }) => {
     const requestBody = LoginDto.createLoginWithCorrectData()
-    const response = await request.post(`${serviceURL}${loginPath}`,{
-      data: requestBody
+    const response = await request.post(`${serviceURL}${loginPath}`, {
+      data: requestBody,
     })
-       expect (response.status()).toBe(200)
+    expect(response.status()).toBe(StatusCodes.OK)
+    const responseBody = await response.text()
+    const jwtPattern = /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
+    expect(responseBody).toMatch(jwtPattern)
   })
 
   test('login with incorrect data', async ({ request }) => {
     const requestBody = LoginDto.createLoginWithIncorrectData()
-    const response = await request.post(`${serviceURL}${loginPath}`,{
-      data: requestBody
+    const response = await request.post(`${serviceURL}${loginPath}`, {
+      data: requestBody,
     })
-    expect (response.status()).toBe(401)
-    console.log('Username:', process.env.USER);
-    console.log('Password:', process.env.PASSWORD);
+    expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
+  })
 
+  test('login with incorrect HTTP method', async ({ request }) => {
+    const requestBody = LoginDto.createLoginWithIncorrectData()
+    const response = await request.get(`${serviceURL}${loginPath}`, {
+      data: requestBody,
+    })
+    expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
   })
 })
